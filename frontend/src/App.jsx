1f278944b8d2ee45ab3d62b7e0c0bf618e08ba61
Abhinab Basnet <abhinab.global@gmail.com>
2026-07-12 11:25:07 -0700

    Created jobsrecommended component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+
 import JobList from './components/JobList'
+import RecommendedJobs from './components/RecommendedJobs'
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1>SkillLink Nepal</h1>
-        <p>Connecting local talent with local opportunity</p>
-      </header>
-      <main>
-        <JobList />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <header className="app-header">
+          <h1>SkillLink Nepal</h1>
+          <p>Connecting local talent with local opportunity</p>
+          <nav className="app-nav">
+            <Link to="/">All Jobs</Link>
+            <Link to="/recommended">Recommended For You</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<JobList />} />
+            <Route path="/recommended" element={<RecommendedJobs />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
